99b16459f994b6181989796f41eb3685f7b0e30d
mrkyaing <mr.kyaing7@gmail.com>
2023-11-19 15:26:00 +0630

    demo for mvc

--- FirstMVCDemo/Controllers/AboutUsController.cs
+++ FirstMVCDemo/Controllers/AboutUsController.cs
@@ -1,23 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FirstMVCDemo.Controllers
 {
+     //[Authorize]
     public class AboutUsController : Controller {
         private readonly IWebHostEnvironment _webHostEnvironment;
         public AboutUsController(IWebHostEnvironment webHostEnvironment)
         {
             _webHostEnvironment = webHostEnvironment;
         }
-
+       
+        //host://port:about/index
         public IActionResult Index()
         {
             return View();
         }
-
+        // //host://port/aboutus/CalculateSum?n1=Hello&n2=Hello
         public IActionResult CalculateSum(int n1,int n2)
         {
             int result = n1 + n2;
             ViewBag.Sum= result;
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Search(string q)
+        {
+            string queryRresult = q;
+
             return View();
         }
 
@@ -36,6 +46,27 @@ namespace FirstMVCDemo.Controllers
         {
             string fullName = $"Hello,{firstName} {lastName}, Welcome to our system";
             ViewBag.Result=fullName;
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Me(string firstName, string lastName)
+        {
+           string FullName = $"Hello,{firstName} {lastName}, Welcome to our system";
+            ViewBag.Result = FullName;
+            return View();
+        }
+        //host://port/aboutus/me
+        [ActionName("me")]
+        public IActionResult DoMe()
+        {
+            return View();
+        }
+        [NonAction]
+        public ViewResult RandomNuber()
+        {
+            Random rnd = new Random();
+            string result= rnd.Next(0, 100).ToString();
+            ViewBag.Result=result;
             return View();
         }
     }
---
+++ FirstMVCDemo/Controllers/StudentController.cs
@@ -0,0 +1,33 @@
+﻿using FirstMVCDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstMVCDemo.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+        IList<StudentModel> students=new List<StudentModel>();
+        [HttpPost]
+        public IActionResult Register(string fristName,string lastName,string DOB,string address,string Gender) {
+            students.Add(new StudentModel
+            {
+                FirstName = fristName,
+                LastName = lastName,
+                DOB =DateOnly.FromDateTime(Convert.ToDateTime(DOB)),
+                Gender=Gender,
+                Address = address
+            });
+            TempData["Info"] = "1 sutdent recrod is successfully registered";
+            TempData["StudentCounts"] = students.Count;
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
