d5bcf5e20487fc4c8e3c42953faea5f88b8fc64b
mrkyaing <mr.kyaing7@gmail.com>
2023-12-16 14:44:32 +0630

    ajax called with jquery

---
+++ WorkOut/Controllers/SimpleController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WorkOut.Models;
+
+namespace WorkOut.Controllers
+{
+    public class SimpleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public string  ShowMeTime()
+        {
+            return DateTime.Now.ToString("yyyy-MM-dd HH:MM:ss");
+        }
+
+        public string ShowMeRandomNnumber()
+        {
+            return new Random().Next(0, 100).ToString();
+        }
+        public IActionResult MakeAnOrder()=>View();
+
+        [HttpPost]
+        public JsonResult MakeAnOrder(OrderModel order)
+        {
+            decimal TotalAmount = order.UnitPrice * order.Quantity;
+            return Json(TotalAmount);
+        }
+
+    }
+}
---
+++ WorkOut/Models/OrderModel.cs
@@ -0,0 +1,10 @@
+﻿namespace WorkOut.Models
+{
+    public class OrderModel
+    {
+        public string  OrderId { get; set; }
+        public int  Quantity { get; set; }
+        public int  UnitPrice { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
