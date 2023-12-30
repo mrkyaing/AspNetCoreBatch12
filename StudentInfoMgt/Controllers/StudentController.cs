928d385799753892d1bdafdcf59629ff4f7662d7
mrkyaing <mr.kyaing7@gmail.com>
2023-12-30 11:34:27 +0630

    crud process with mvc

--- StudentInfoMgt/Controllers/StudentController.cs
+++ StudentInfoMgt/Controllers/StudentController.cs
@@ -65,5 +65,63 @@ namespace StudentInfoMgt.Controllers
            }).ToList();
             return View(students);//passing the data to the View 
         }
+
+        public IActionResult Delete(string id)
+        {
+            StudentEntity student=_applicationDb.Students.Where(w=>w.Id==id).SingleOrDefault();
+            if (student != null)
+            {
+            _applicationDb.Remove(student);//
+              _applicationDb.SaveChanges();
+              TempData["info"] = "successfully deleted the record from the system";
+            }          
+         return RedirectToAction("list");
+        }
+    
+        public IActionResult Edit(string id)
+        {
+           StudentViewModel svm= _applicationDb.Students.Where(w => w.Id == id).Select(s=>new StudentViewModel
+           {
+               Id = s.Id,
+               Name = s.Name,
+               Code = s.Code,
+               Phone = s.Phone,
+               Email = s.Email,
+               NRC = s.NRC,
+               FatherName = s.FatherName,
+               DOB = s.DOB,
+               Address = s.Address
+           }).SingleOrDefault();
+            return View(svm);
+        }
+        [HttpPost]
+        public IActionResult Update(StudentViewModel svm)
+        {
+            try
+            {
+                //Data transfer from viewmodel to dataModel(studentEntity) in here
+                StudentEntity studentEntity = new StudentEntity()
+                {
+                    Id =svm.Id,//this id is primary key in db if so, it will be update this key
+                    Name = svm.Name,
+                    Email = svm.Email,
+                    Phone = svm.Phone,
+                    NRC = svm.NRC,
+                    FatherName = svm.FatherName,
+                    DOB = svm.DOB,
+                    Address = svm.Address,
+                    Code = svm.Code
+                };
+                _applicationDb.Students.Update(studentEntity);//update the studetEntity to the DBSET in here 
+                _applicationDb.SaveChanges();//updating the recrod to the connected database .
+                TempData["info"] = "Update Successfully the recrod to the system";
+            }
+            catch (Exception e)
+            {
+                TempData["info"] = "error occur when updating  the recrod to the system";
+                throw;
+            }
+            return RedirectToAction("List");
+        }
     }
 }
